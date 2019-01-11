76a448c6b64930b8d5a64efc43db7fde7ebbd6e9
Michael Insel <michael@insel.email>
2019-01-11 21:51:58 +0100

    Implement more methods
    This implements the following methods
    
    * downstream_current to get the current downstream rate
    * upstream_current to get the current upstream rate
    * interface_upgrade to get the update state for the device

--- cmd/check_fritz/check_downstream.go
+++ cmd/check_fritz/check_downstream.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/mcktr/check_fritz/pkg/fritz"
 )
@@ -34,6 +35,39 @@ func CheckDownstreamMax(hostname, port string, username string, password string)
 	downstream = downstream * 8 / 1000000
 
 	fmt.Print("OK - Max Downstream: " + strconv.Itoa(downstream) + " Mbit/s \n")
+
+	GlobalReturnCode = exitOk
+}
+
+func CheckDownstreamCurrent(hostname string, port string, username string, password string) {
+	soapReq := fritz.NewSoapRequest(username, password, hostname, port, "/upnp/control/wancommonifconfig1", "WANCommonInterfaceConfig", "X_AVM-DE_GetOnlineMonitor")
+	fritz.AddSoapRequestVariable(&soapReq, fritz.NewSoapRequestVariable("NewSyncGroupIndex", "0"))
+
+	err := fritz.DoSoapRequest(&soapReq)
+
+	if HandleError(err) {
+		return
+	}
+
+	var resp = fritz.GetWANCommonInterfaceOnlineMonitorResponse{}
+
+	err = fritz.HandleSoapRequest(&soapReq, &resp)
+
+	if HandleError(err) {
+		return
+	}
+
+	downstreamWithHistory := strings.Split(resp.Newds_current_bps, ",")
+
+	downstream, err := strconv.ParseFloat(downstreamWithHistory[0], 32)
+
+	if HandleError(err) {
+		return
+	}
+
+	downstream = downstream * 8 / 1000000
+
+	fmt.Print("OK - Current Downstream: " + fmt.Sprintf("%f", downstream) + " Mbit/s \n")
 
 	GlobalReturnCode = exitOk
 }
--- cmd/check_fritz/check_upstream.go
+++ cmd/check_fritz/check_upstream.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/mcktr/check_fritz/pkg/fritz"
 )
@@ -34,6 +35,39 @@ func CheckUpstreamMax(hostname, port string, username string, password string) {
 	upstream = upstream * 8 / 1000000
 
 	fmt.Print("OK - Max Upstream: " + strconv.Itoa(upstream) + " Mbit/s \n")
+
+	GlobalReturnCode = exitOk
+}
+
+func CheckUpstreamCurrent(hostname string, port string, username string, password string) {
+	soapReq := fritz.NewSoapRequest(username, password, hostname, port, "/upnp/control/wancommonifconfig1", "WANCommonInterfaceConfig", "X_AVM-DE_GetOnlineMonitor")
+	fritz.AddSoapRequestVariable(&soapReq, fritz.NewSoapRequestVariable("NewSyncGroupIndex", "0"))
+
+	err := fritz.DoSoapRequest(&soapReq)
+
+	if HandleError(err) {
+		return
+	}
+
+	var resp = fritz.GetWANCommonInterfaceOnlineMonitorResponse{}
+
+	err = fritz.HandleSoapRequest(&soapReq, &resp)
+
+	if HandleError(err) {
+		return
+	}
+
+	upstreamWithHistory := strings.Split(resp.Newus_current_bps, ",")
+
+	upstream, err := strconv.ParseFloat(upstreamWithHistory[0], 32)
+
+	if HandleError(err) {
+		return
+	}
+
+	upstream = upstream * 8 / 1000000
+
+	fmt.Print("OK - Current Downstream: " + fmt.Sprintf("%f", upstream) + " Mbit/s \n")
 
 	GlobalReturnCode = exitOk
 }
